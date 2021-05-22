b2da7905cdfe13e9a3aba94ee0b52aadbe223721
Dan <danyi1212@gmail.com>
2021-05-23 00:49:32 +0300

    Changed build scripts venv creation

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ mkdir build
 tar -cvzf build/project.tar.gz -C project_template/ .
 tar -cvzf build/app.tar.gz -C app_template/ .
 
-if not exist ./venv virtualenv venv
+if not exist ./venv python -m venv venv
 call ./venv/Scripts/activate.bat
 pip install django ipython docutils django-debug-toolbar django-cors-headers django-simple-history django-background-tasks djangorestframework django-filter drf-yasg djangorestframework-simplejwt drf-access-policy
 pip freeze > build/requirements.txt
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ mkdir build
 tar -cvzf build/project.tar.gz -C project_template/ .
 tar -cvzf build/app.tar.gz -C app_template/ .
 
-[[ -d ./venv ]] || virtualenv venv
+[[ -d ./venv ]] || python -m venv venv
 ./venv/Scripts/activate
 pip install django ipython docutils django-debug-toolbar django-cors-headers django-simple-history django-background-tasks djangorestframwrork django-filter drf-yasg djangorestframework-simplejwt drf-access-policy
 pip freeze > build/requirements.txt
