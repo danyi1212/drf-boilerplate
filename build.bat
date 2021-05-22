fa52c98a0d951d4ddc4af399590f2e921cb2f7e4
Dan <danyi1212@gmail.com>
2021-05-23 00:41:02 +0300

    Replaced drf-yasg in builds

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ tar -cvzf build/app.tar.gz -C app_template/ .
 
 if not exist ./venv virtualenv venv
 call ./venv/Scripts/activate.bat
-pip install django ipython docutils django-debug-toolbar django-cors-headers django-simple-history django-background-tasks djangorestframework django-filter drf-yasg djangorestframework-simplejwt drf-access-policy
+pip install django ipython docutils django-debug-toolbar django-cors-headers django-simple-history django-background-tasks djangorestframework django-filter drf-spectacular djangorestframework-simplejwt drf-access-policy
 pip freeze > build/requirements.txt
--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ tar -cvzf build/app.tar.gz -C app_template/ .
 
 [[ -d ./venv ]] || virtualenv venv
 ./venv/Scripts/activate
-pip install django ipython docutils django-debug-toolbar django-cors-headers django-simple-history django-background-tasks djangorestframwrork django-filter drf-yasg djangorestframework-simplejwt drf-access-policy
+pip install django ipython docutils django-debug-toolbar django-cors-headers django-simple-history django-background-tasks djangorestframwrork django-filter drf-spectacular djangorestframework-simplejwt drf-access-policy
 pip freeze > build/requirements.txt
